2454dd9034622432a4d4f07d55e6f6e561b55823
David Morasz <david.morasz@meso.design>
2021-02-02 00:10:39 +0100

    ^ keep naming consistent, the per-project build targets are in Nuke.Targets now

--- build.ps1
+++ build.ps1
@@ -13,7 +13,7 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 # CONFIGURATION
 ###########################################################################
 
-$BuildProjectFile = "$PSScriptRoot\NukeTargets\_build.csproj"
+$BuildProjectFile = "$PSScriptRoot\Nuke.Targets\_build.csproj"
 $TempDirectory = "$PSScriptRoot\\.tmp"
 
 $DotNetGlobalFile = "$PSScriptRoot\\global.json"
--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 # CONFIGURATION
 ###########################################################################
 
-BUILD_PROJECT_FILE="$SCRIPT_DIR/NukeTargets/_build.csproj"
+BUILD_PROJECT_FILE="$SCRIPT_DIR/Nuke.Targets/_build.csproj"
 TEMP_DIRECTORY="$SCRIPT_DIR//.tmp"
 
 DOTNET_GLOBAL_FILE="$SCRIPT_DIR//global.json"
